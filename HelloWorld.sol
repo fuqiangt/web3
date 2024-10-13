5596c28577d6b31245800ff605e2f3139c7666cf
fuqiangt <941867223@qq.com>
2024-10-13 22:43:26 +0800

    提交不同变量类型

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -2,12 +2,25 @@
 
 //智能合约的许可协议
 pragma solidity ^0.8.7;
+
 contract HelloWorld {
+    // 状态变量，放在区域链上
     string public name;
-    int public version;
+    uint public version;
 
     constructor() {
-        name = 'web3';
+        name = "web3";
         version = 1;
+    }
+
+    function sayName() public view returns (string memory) {
+        return name;
+    }
+
+    function changeName(string memory _name) public  {
+        // 局部变量
+        uint num = 123;
+        version = num;
+        name = _name;        
     }
 }
